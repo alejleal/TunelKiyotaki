b6cdf58b196c49398c801728f332ad7e04902d6f
alejleal <alejleal@ucm.es>
2022-04-02 19:11:39 +0200

    Cambio de tabs a espacios

--- 02_sol_skel.py
+++ 02_sol_skel.py
@@ -12,63 +12,63 @@ NORTH = 0
 NCARS = 10
 
 class Monitor():
-	def __init__(self):
-	        self.mutex = Lock()
-	        self.manager = Manager()
-	        self.cars = Value('i', 0) #Numero de coches dentro del tunel
-	        self.actual_direction = Value('i', 0) #Direccion en la que circulan o pueden circular los coches dentro del tunel
-	        self.open_north = Condition(self.mutex)
-	        self.open_south = Condition(self.mutex)
+    def __init__(self):
+        self.mutex = Lock()
+        self.manager = Manager()
+        self.cars = Value('i', 0) #Numero de coches dentro del tunel
+        self.actual_direction = Value('i', 0) #Direccion en la que circulan o pueden circular los coches dentro del tunel
+        self.open_north = Condition(self.mutex)
+        self.open_south = Condition(self.mutex)
 
-	def wants_enter(self, direction):
-		self.mutex.acquire()
-		if direction == NORTH:
-			self.open_north.wait_for(lambda: self.actual_direction.value == direction or self.cars.value == 0)
-			self.actual_direction = NORTH
-		else:
-			self.open_south.wait_for(lambda: self.actual_direction.value == direction or self.cars.value == 0)
-			self.actual_direction = SOUTH
-	
-		self.cars.value += 1
-		self.mutex.release()
+    def wants_enter(self, direction):
+        self.mutex.acquire()
+        if direction == NORTH:
+            self.open_north.wait_for(lambda: self.actual_direction.value == direction or self.cars.value == 0)
+            self.actual_direction = NORTH
+        else:
+            self.open_south.wait_for(lambda: self.actual_direction.value == direction or self.cars.value == 0)
+            self.actual_direction = SOUTH
+    
+        self.cars.value += 1
+        self.mutex.release()
 
-	def leaves_tunnel(self, direction):
-		self.mutex.acquire()
-		self.cars.value -= 1
-		if self.cars.value == 0: # No hay coches, podemos cambiar la direccion
-			if direction == NORTH:            		
-				self.open_south.notify_all()
-				self.open_north.notify_all()
-			else:
-				self.open_north.notify_all()
-				self.open_south.notify_all()
-				
-		self.mutex.release()
+    def leaves_tunnel(self, direction):
+        self.mutex.acquire()
+        self.cars.value -= 1
+        if self.cars.value == 0: # No hay coches, podemos cambiar la direccion
+            if direction == NORTH:            		
+                self.open_south.notify_all()
+                self.open_north.notify_all()
+            else:
+                self.open_north.notify_all()
+                self.open_south.notify_all()
+                
+        self.mutex.release()
 
 def delay(n=3):
-	time.sleep(random.random()*n)
+    time.sleep(random.random()*n)
 
 def car(cid, direction, monitor):
-	print(f"car {cid} direction {direction} created")
-	delay(6)
-	print(f"car {cid} heading {direction} wants to enter")
-	monitor.wants_enter(direction)
-	print(f"car {cid} heading {direction} enters the tunnel")
-	delay(3)
-	print(f"car {cid} heading {direction} leaving the tunnel")
-	monitor.leaves_tunnel(direction)
-	print(f"car {cid} heading {direction} out of the tunnel")
+    print(f"car {cid} direction {direction} created")
+    delay(6)
+    print(f"car {cid} heading {direction} wants to enter")
+    monitor.wants_enter(direction)
+    print(f"car {cid} heading {direction} enters the tunnel")
+    delay(3)
+    print(f"car {cid} heading {direction} leaving the tunnel")
+    monitor.leaves_tunnel(direction)
+    print(f"car {cid} heading {direction} out of the tunnel")
 
 def main():
-	monitor = Monitor()	
-	cid = 0
-	for _ in range(NCARS):
-		direction = NORTH if random.randint(0,1)==1  else SOUTH
-		cid += 1
-		p = Process(target=car, args=(cid, direction, monitor))
-		p.start()
-		time.sleep(random.expovariate(1/0.5)) # a new car enters each 0.5s
+    monitor = Monitor()	
+    cid = 0
+    for _ in range(NCARS):
+        direction = NORTH if random.randint(0,1)==1  else SOUTH
+        cid += 1
+        p = Process(target=car, args=(cid, direction, monitor))
+        p.start()
+        time.sleep(random.expovariate(1/0.5)) # a new car enters each 0.5s
 
 
 if __name__ == "__main__":
-	main()
+    main()
